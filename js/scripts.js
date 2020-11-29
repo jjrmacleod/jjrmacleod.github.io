c9f669d46e40abbc65494e9db13390b0570d5a31
Andy Gaskell <ag@ssofb.co.uk>
2020-11-29 12:26:59 +0000

    gallery

--- js/scripts.js
+++ js/scripts.js
@@ -359,4 +359,31 @@
 		$(this).blur();
 	});
 
+    /* magnific gallery */
+	$('.zoom-gallery').magnificPopup({
+		delegate: 'a',
+		type: 'image',
+		closeOnContentClick: false,
+		closeBtnInside: false,
+		mainClass: 'mfp-with-zoom mfp-img-mobile',
+		image: {
+			verticalFit: true,
+			titleSrc: function(item) {
+				return item.el.attr('title');
+			}
+		},
+		gallery: {
+			enabled: true
+		},
+		zoom: {
+			enabled: true,
+			duration: 300, // don't foget to change the duration also in CSS
+			opener: function(element) {
+				return element.find('img');
+			}
+		}
+		
+	});
+
+
 })(jQuery);
